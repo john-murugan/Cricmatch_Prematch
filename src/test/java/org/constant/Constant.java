9f82d8f7bbd8dba32d3f4fe7ff89df1c43ffd37a
bharath <bharath@R3NRCX06F801136>
2024-06-19 13:51:11 +0530

    Committed to push the project structure

---
+++ src/test/java/org/constant/Constant.java
@@ -0,0 +1,9 @@
+package org.constant;
+
+public class Constant {
+	public static String APP_URL;
+	public static String BROWSER;
+	public static String USERNAME;
+	public static String PASSWORD;
+
+}
---
+++ src/test/resources/CricmatchFeature/Cricmatch.feature
@@ -0,0 +1,3 @@
+Feature: Launch the browser
+Scenario: login and launch browser
+Given Launnch broswer
